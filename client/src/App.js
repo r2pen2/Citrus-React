625615fd9f3ca1abc409963a1ba6d96442c34c8e
r2pen2 <joedobbelaar@gmail.com>
2022-06-09 20:23:13 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Dashboard from "./components/dashboard/Dashboard"
+import Navbar from "./components/navbar/Navbar"
+
+function App() {
+  return (
+  <div>
+    <Navbar name="Joseph Dobbelaar"/>
+    <Dashboard name="Joseph Dobbelaar"/>
+  </div>
+  )
+}
+
+export default App;
---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+import "./dashboard.sass"
+
+export default function Dashboard({ name }) {
+  return (
+    <div className="dashboard">{name}</div>
+  )
+}
