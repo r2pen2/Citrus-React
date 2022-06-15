0318be7c19f9ad88b2d3291680c2b953a41c3ff7
r2pen2 <joedobbelaar@gmail.com>
2022-06-15 13:14:20 -0400

    Formatter desc.

--- client/src/api/formatter.js
+++ client/src/api/formatter.js
@@ -1,3 +1,5 @@
+// Builds a formatter
+// Currently just does USD, but we can add a param to control this later
 export default new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
