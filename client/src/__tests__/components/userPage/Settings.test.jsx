44e45628378ceea1cc07e3b18788dd246101eaf7
r2pen2 <joedobbelaar@gmail.com>
2022-06-19 17:07:13 -0400

    Drawer with items that change window.location.hash and update content

--- client/src/__tests__/components/userPage/Settings.test.jsx
+++ client/src/__tests__/components/userPage/Settings.test.jsx
@@ -11,6 +11,33 @@ describe("Settings render tests", () => {
         const wrapper = screen.getByTestId("user-settings-wrapper");
         expect(wrapper).toBeVisible();
     });
+
+    test('Paper renders', () => {
+        render(<Settings user={testUser}/>);
+        const paper = screen.getByTestId("settings-paper");
+        expect(paper).toBeVisible();
+    });
+
+    test('Drawer renders', () => {
+        render(<Settings user={testUser}/>);
+        const drawer = screen.getByTestId("settings-drawer");
+        expect(drawer).toBeVisible();
+        const title = screen.getByTestId("settings-drawer-title");
+        expect(drawer).toContainElement(title);
+    });
+
+    test('All drawer items render', () => {
+        render(<Settings user={testUser}/>);
+        const drawer = screen.getByTestId("settings-drawer");
+        const account = screen.getByTestId("drawer-item-Account");
+        const appearance = screen.getByTestId("drawer-item-Appearance");
+        const connections = screen.getByTestId("drawer-item-Connections");
+        const security = screen.getByTestId("drawer-item-Security");
+        expect(drawer).toContainElement(account);
+        expect(drawer).toContainElement(appearance);
+        expect(drawer).toContainElement(connections);
+        expect(drawer).toContainElement(security);
+    });
 });
 
 describe("Settings function tests", () => {
--- client/src/components/userPage/settings/Settings.jsx
+++ client/src/components/userPage/settings/Settings.jsx
@@ -1,4 +1,13 @@
+// Style imports
 import "./settings.scss"
+
+// Library imports
+import { ListItem, Typography, Drawer, List, Paper, Divider, ListItemIcon, ListItemText } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import SecurityIcon from '@mui/icons-material/Security';
+import LinkIcon from '@mui/icons-material/Link';
+import { useState } from 'react';
 
 export default function Settings({ user }) {
 
@@ -9,7 +18,93 @@ export default function Settings({ user }) {
         document.title = "Citrus | Settings";
     }
 
+    const [hash, setHash] = useState("");           // The current window.location.hash (for displaying the correct page)
+
+    /**
+     * Set url hash to menu item path
+     * @param {String} path href of menu item 
+     */
+    function handleDrawerClick(path) {
+        setHash(path);
+        window.location.hash = path;
+    }
+    
+    function getSettingsPageByHash(currentUser) {
+        console.log(hash);
+        switch (hash) {
+            case "#account":
+                return <div>Account</div>;
+            case "#appearance":
+                return <div>Appearance</div>;
+            case "#connections":
+                return <div>Connections</div>;
+            case "#security":
+                return <div>Security</div>;
+            default:
+                return <div>Account</div>
+        }
+    }
+
+    // List of menu items to be displayed on left. Easy to add/remove/edit items.
+    const menuItems = [
+        {
+            text: 'Account',
+            icon: <AccountCircleIcon color="secondary"/>,
+            path: '#account'
+        },
+        {
+            text: 'Appearance',
+            icon: <ColorLensIcon color="secondary"/>,
+            path: '#appearance'
+        },
+        {
+            text: 'Connections',
+            icon: <LinkIcon color="secondary"/>,
+            path: '#connections'
+        },
+        {
+            text: 'Security',
+            icon: <SecurityIcon color="secondary"/>,
+            path: '#security'
+        },
+        
+    ]
+
     return (
-      <div className="user-settings" data-testid="user-settings-wrapper">Settings</div>
+      <div className="user-settings" data-testid="user-settings-wrapper">
+        <Paper className="settings-content" data-testid="settings-paper" elevation={12} sx={{ backgroundColor: '#fafafa', borderRadius: "10px"}}>
+
+            
+            { /* side drawer */ }
+            <Drawer
+                className="side-drawer"
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: "drawer-paper" }}
+                data-testid="settings-drawer"
+            >
+                
+                { /* list / links */ }
+                <List>
+                    <ListItem key="title">
+                        <Typography variant="h5" data-testid="settings-drawer-title">
+                            User Settings
+                        </Typography>
+                    </ListItem>
+                    <Divider />
+                    {menuItems.map(item => (
+                      <ListItem button key={item.text} onClick={() => handleDrawerClick(item.path)} data-testid={"drawer-item-" + item.text}>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                      </ListItem>  
+                    ))}
+                </List>
+
+            </Drawer>
+
+            { /* Then display the right page based on the url */ }
+            { getSettingsPageByHash(user) }
+        </Paper>
+      </div>
     )
 }
