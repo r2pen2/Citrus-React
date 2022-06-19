5fb9dd60385be7cd164e015cb4c786e710c0d76b
oliver-risch <oliver.risch@gmail.com>
2022-06-19 15:53:16 -0400

    Adding (rough) first page for new transactions

---
+++ client/src/components/dashboard/newTransaction/NewTransaction.jsx
@@ -0,0 +1,14 @@
+// Style imports
+import "./newtransaction.scss";
+
+// Library imports
+// import { Stack, Box } from "@mui/material"
+
+// Component imports
+import GroupOrIndividualBox from "./groupOrIndividual/GroupOrIndividual";
+// import Transactions from "./transactions/Transactions";
+// import Analytics from "./analytics/Analytics";
+
+export default function NewTransaction(user) {
+  return <GroupOrIndividualBox />;
+}
