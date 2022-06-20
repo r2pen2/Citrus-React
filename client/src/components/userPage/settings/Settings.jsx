cb9d64f89e66c2fd6330da116fbda6299ed3ae95
r2pen2 <joedobbelaar@gmail.com>
2022-06-19 23:26:17 -0400

    Removed title and added blue bar on left

--- client/src/components/userPage/settings/Settings.jsx
+++ client/src/components/userPage/settings/Settings.jsx
@@ -109,17 +109,21 @@ export default function Settings({ user }) {
                 
                 { /* list / links */ }
                 <List>
-                    <ListItem key="title">
-                        <Typography variant="h5" data-testid="settings-drawer-title">
-                            User Settings
-                        </Typography>
-                    </ListItem>
-                    <Divider />
                     {menuItems.map(item => (
-                      <ListItem button key={item.text} onClick={() => handleDrawerClick(item.path)} data-testid={"drawer-item-" + item.text} className={isElementActive(item.text) ? "active" : ""}>
-                        <ListItemIcon>{item.icon}</ListItemIcon>
-                        <ListItemText primary={item.text} />
-                      </ListItem>  
+                    <div>
+                        <ListItem 
+                            button 
+                            key={item.text} 
+                            onClick={() => handleDrawerClick(item.path)} 
+                            data-testid={"drawer-item-" + item.text} 
+                            className={isElementActive(item.text) ? "active" : ""}
+                            display="flex"
+                        >
+                          <ListItemIcon>{item.icon}</ListItemIcon>
+                          <ListItemText primary={item.text} />
+                        </ListItem>  
+                        <Divider variant="middle" />
+                    </div>
                     ))}
                 </List>
 
