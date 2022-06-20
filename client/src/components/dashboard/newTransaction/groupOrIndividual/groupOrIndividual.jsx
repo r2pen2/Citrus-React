8c450cee9f340156d8512147a4f5c5b32b8167f7
oliver-risch <oliver.risch@gmail.com>
2022-06-20 14:42:04 -0400

    latest button updates

--- client/src/components/dashboard/newTransaction/groupOrIndividual/groupOrIndividual.jsx
+++ client/src/components/dashboard/newTransaction/groupOrIndividual/groupOrIndividual.jsx
@@ -27,8 +27,8 @@ export default function GroupOrIndividualBox() {
       >
         <Card
           variant="outlined"
-          sx={getLeftCardStyle()}
-          data-testid="owe-card-card-element"
+          sx={getCardStyle("left")}
+          data-testid="group-or-individual-testid"
         >
           <CardActionArea>
             <CardContent>
@@ -47,62 +47,38 @@ export default function GroupOrIndividualBox() {
           </CardActionArea>
         </Card>
 
-        <div className="binary-choice-button left">
-          <Typography>Group</Typography>
-        </div>
+        <Card
+          variant="outlined"
+          sx={getCardStyle("right")}
+          data-testid="group-or-individual-testid"
+        >
+          <CardActionArea>
+            <CardContent>
+              <Stack direction="row" alignItems="center">
+                <Typography
+                  variant="subtitle1"
+                  component="div"
+                  marginLeft="5px"
+                  marginTop="2px"
+                  color="white"
+                >
+                  Group
+                </Typography>
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Stack>
     </div>
   );
 }
 
-{
-  /* <Card className="binary-choice-button right" variant="outlined">
-<CardActionArea>
-  <CardContent>
-    <Typography>Individual</Typography>
-  </CardContent>
-</CardActionArea>
-</Card> */
-}
-
-function getLeftCardStyle() {
+function getCardStyle(side) {
   return {
-    backgroundColor: "rgba(176, 200, 86, 0.8)",
-    width: "40%",
-    borderRadius: "15px",
-    marginBottom: "10px",
-  };
-}
-
-function getRightCardStyle() {
-  return {
-    backgroundColor: "rgba(234, 66, 54, 0.5)",
-    width: "40%",
-    borderRadius: "15px",
-    marginBottom: "10px",
-  };
-}
-
-function getCardStyle() {
-  return {
-    widthi: "60%",
+    backgroundColor:
+      side === "left" ? "rgba(176, 200, 86, 0.8)" : "rgba(234, 66, 54, 0.5)",
+    width: "60%",
     borderRadius: "5px",
     marginBottom: "10px",
   };
 }
-// {
-/* <div data-testid={"owe-card-" + (credit.positive ? "positive" : "negative")}>
-<Typography sc={{ fontSize: 14}} color="text-secondary" gutterBottom>{credit.positive ? "Owe Me" : "I Owe"} ❯</Typography>
-<Card variant="outlined" sx={getCardStyle(credit)} data-testid="owe-card-card-element">
-    <CardActionArea>
-        <CardContent>
-            <Typography variant="h5" component="div" color="white">{formatter.format(credit.amount)}</Typography>
-                <Stack direction="row" alignItems="center">
-                    <GroupsIcon fontSize="large" sx={{ color: "white" }}/>
-                    <Typography variant="subtitle1" component="div" marginLeft="5px" marginTop="2px" color="white">To {credit.numPeople} people</Typography>
-                </Stack>
-        </CardContent>
-    </CardActionArea>
-</Card>
-</div> */
-// }
