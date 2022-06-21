ad8ef7f227756b59ed616492a2b4669e699e6f87
r2pen2 <joedobbelaar@gmail.com>
2022-06-21 18:56:37 -0400

    Fixed dashboard owe cards not covering entire screen

--- client/src/components/dashboard/home/oweCard/OweCard.jsx
+++ client/src/components/dashboard/home/oweCard/OweCard.jsx
@@ -25,7 +25,7 @@ function getCardStyle(credit) {
     : "rgba(234, 66, 54, 0.5)";
   return {
     backgroundColor: bgColor,
-    width: "60%",
+    width: "100%",
     borderRadius: "5px",
     marginBottom: "10px",
   };
@@ -35,35 +35,38 @@ export default function OweCard({ credit }) {
   return (
     <div
       data-testid={"owe-card-" + (credit.positive ? "positive" : "negative")}
+      className="owe-card-container"
     >
       <Typography sc={{ fontSize: 14 }} color="text-secondary" gutterBottom>
         {credit.positive ? "Owe Me" : "I Owe"} ❯
       </Typography>
-      <Card
-        variant="outlined"
-        sx={getCardStyle(credit)}
-        data-testid="owe-card-card-element"
-      >
-        <CardActionArea>
-          <CardContent>
-            <Typography variant="h5" component="div" color="white">
-              {formatter.format(credit.amount)}
-            </Typography>
-            <Stack direction="row" alignItems="center">
-              <GroupsIcon fontSize="large" sx={{ color: "white" }} />
-              <Typography
-                variant="subtitle1"
-                component="div"
-                marginLeft="5px"
-                marginTop="2px"
-                color="white"
-              >
-                To {credit.numPeople} people
+      <div className="card-wrapper">
+        <Card
+          variant="outlined"
+          sx={getCardStyle(credit)}
+          data-testid="owe-card-card-element"
+        >
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h5" component="div" color="white">
+                {formatter.format(credit.amount)}
               </Typography>
-            </Stack>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+              <Stack direction="row" alignItems="center">
+                <GroupsIcon fontSize="large" sx={{ color: "white" }} />
+                <Typography
+                  variant="subtitle1"
+                  component="div"
+                  marginLeft="5px"
+                  marginTop="2px"
+                  color="white"
+                >
+                  To {credit.numPeople} people
+                </Typography>
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
     </div>
   );
 }
