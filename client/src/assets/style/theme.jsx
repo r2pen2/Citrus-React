8a9a8d21813afd91627028b46a0d6966c0eed886
oliver-risch <oliver.risch@gmail.com>
2022-06-21 20:40:17 -0400

    add back in defaults

--- client/src/assets/style/theme.jsx
+++ client/src/assets/style/theme.jsx
@@ -1,6 +1,7 @@
 import { createTheme } from "@mui/material/styles";
 
 export default createTheme({
+  // declare re-used variables
   palette: {
     type: "light",
     citrusGreen: {
@@ -25,10 +26,22 @@ export default createTheme({
       main: "#E5E5E5",
     },
     primary: {
-      main: "#B0C856CC",
+      main: "#B0C856CC", // aka citrusGreen
     },
     secondary: {
-      main: "#FDB90F",
+      main: "#FDB90F", // aka citrusOrange
+    },
+    error: {
+      main: "#EA4236", // aka citrusRed
+    },
+    success: {
+      main: "#B0C856CC", // aka citrusGreen
+    },
+    warning: {
+      main: "#FDB90F", // aka citrusOrange
+    },
+    info: {
+      main: "#F2DF56CC", // aka citrusYellow
     },
   },
 });
