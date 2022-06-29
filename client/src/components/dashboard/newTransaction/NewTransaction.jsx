fce94bcf56bff8975a2d15f74575ff3991aa0960
oliver-risch <oliver.risch@gmail.com>
2022-06-28 23:49:41 -0400

    syncing new paths with bookmarks

--- client/src/components/dashboard/newTransaction/NewTransaction.jsx
+++ client/src/components/dashboard/newTransaction/NewTransaction.jsx
@@ -5,24 +5,6 @@ import "./newtransaction.scss";
 import { Route, Routes } from "react-router-dom";
 
 // Component imports
-<<<<<<< HEAD
-import Diagnostics from "./diagnostics/Diagnostics";
-import Iou from "./iou/Iou";
-import Communal from "./communal/Communal";
-
-export default function NewTransaction({ user }) {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Diagnostics />} />
-        <Route path="/diagnostics" element={<Diagnostics />} />
-        <Route path="/iou" element={<Iou />} />
-        <Route path="/communal" element={<Communal />} />
-        {/* <Route path="/group-or-one-time" element={<GroupOrOneTimeBox />} />
-        <Route path="/transaction-or-iou" element={<TransactionOrIouBox />} /> */}
-      </Routes>
-    </div>
-=======
 import GroupOrOneTimeBox from "./groupOrOneTime/GroupOrOneTime";
 import BookmarkController from "./bookmarkController/BookmarkController";
 // import Transactions from "./transactions/Transactions";
@@ -31,11 +13,11 @@ import BookmarkController from "./bookmarkController/BookmarkController";
 export default function NewTransaction({ user }) {
   return (
     <Routes>
-      <Route path="/" element={<GroupOrOneTimeBox />} />
-      <Route path="/group-or-one-time" element={<GroupOrOneTimeBox />} />
+      <Route path="/" element={<Diagnostics />} />
+      <Route path="/diagnostics" element={<Diagnostics />} />
+      <Route path="/iou" element={<Iou />} />
+      <Route path="/communal" element={<Communal />} />
       <Route path="/bookmarks/*" element={<BookmarkController />} />
-      {/* <Route pate="/transaction-or-iou" element={<TransactionOrIouBox />} /> */}
     </Routes>
->>>>>>> 1c3f56eec3abc4bb166385ade61d7b7871412fb1
   );
 }
