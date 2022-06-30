83d74b1e4d60385f9ffad7840c0c1fc785c33bc0
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-06-29 20:16:14 -0400

    Firebase setup!

---
+++ client/src/firebase.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/authentication';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+/**
+ * The apiKey in this configuration snippet just identifies your
+ * Firebase project on the Google servers. It is not a security risk 
+ * for someone to know it. In fact, it is necessary for them to know 
+ * it, in order for them to interact with your Firebase project. This 
+ * same configuration data is also included in every iOS and Android 
+ * app that uses Firebase as its backend.
+ */
+const firebaseConfig = {
+  apiKey: "AIzaSyDDfB92e-rvY08msCFedG-t9p1tP8mjW70",
+  authDomain: "citrus-react.firebaseapp.com",
+  projectId: "citrus-react",
+  storageBucket: "citrus-react.appspot.com",
+  messagingSenderId: "735310504995",
+  appId: "1:735310504995:web:5093441ef61a7b5f6b3d96",
+  measurementId: "G-F269165D6E"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
