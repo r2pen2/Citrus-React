4f3188d33d31036b0f3c9691f298606e9c0541b1
oliver-risch <oliver.risch@gmail.com>
2022-07-02 13:00:47 -0400

    building out Iou using diagnostics as template

--- client/src/components/dashboard/newTransaction/iou/Iou.jsx
+++ client/src/components/dashboard/newTransaction/iou/Iou.jsx
@@ -13,14 +13,40 @@ export default function Iou({ user }) {
   const params = new URLSearchParams(window.location.search);
   const affiliation = params.get("affiliation");
 
-  switch (state.step) {
-    case 1:
-      return (
-        <dif>
-          insert IOU form for a transaction with affiliation: {affiliation}
-        </dif>
-      );
-    default:
-      return <div>error</div>;
+  function getPageContent() {
+    switch (step) {
+      case 0:
+        window.location = "/dashboard/diagnostics";
+        break;
+      case 1:
+        return (
+          <WhomQuestion
+            nextStep={nextStep}
+            prevStep={prevStep}
+            updateValue={updateValue}
+          />
+        );
+      case 2:
+        return (
+          <AmountQuestion
+            nextStep={nextStep}
+            prevStep={prevStep}
+            updateValue={updateValue}
+          />
+        );
+      case 3:
+        return (
+          <TitleDescQuestion
+            nextStep={nextStep}
+            prevStep={prevStep}
+            updateValue={updateValue}
+          />
+        );
+      case 4:
+        return <div>Submit a transaction</div>;
+      default:
+        return <LoadingScreen />;
+    }
   }
+  return <div>{getPageContent()}</div>;
 }
