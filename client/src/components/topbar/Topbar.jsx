d0b567d4e5cd231ca390e8464b5a02a4d4bd611f
oliver-risch <oliver.risch@gmail.com>
2022-07-05 19:39:42 -0400

    got rid of errors with topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,19 +2,28 @@
 import "./topbar.scss";
 
 // Library imports
-import { useState, useEffect } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Stack, Tooltip, Avatar, MenuItem, Menu } from "@mui/material";
+import { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Stack,
+  Tooltip,
+  Avatar,
+  MenuItem,
+  Menu,
+} from "@mui/material";
 
 // Component Imports
 import BlackLogo from "./assets/LogoBlack.png";
 import LogoutIcon from "@mui/icons-material/Logout";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import BookmarksIcon from '@mui/icons-material/Bookmarks';
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
 
 // API Imports
-import { signOutUser } from '../../api/firebase'
-import { getDisplayNameById, getPhotoUrlById } from "../../api/dbManager"
-
+import { signOutUser } from "../../api/firebase";
+import { getDisplayNameById, getPhotoUrlById } from "../../api/dbManager";
 
 /**
  * Gets a user's profile picture url
@@ -26,50 +35,45 @@ function getPfp(user) {
 }
 
 export default function Topbar() {
-    
-    const user = JSON.parse(localStorage.getItem("citrus:user"));
-    const [userDisplayName, setUserDisplayName] = useState("");
-    const [userPhotoUrl, setUserPhotoUrl] = useState("");
- 
-    /**
-     * Replace blank values with user details from DB
-     */
-    async function fetchUserData() {
-        let name = await getDisplayNameById(user.uid);
-        setUserDisplayName(name);
-        let url = await getPhotoUrlById(user.uid);
-        setUserPhotoUrl(url);
+  const user = JSON.parse(localStorage.getItem("citrus:user"));
+  const [userDisplayName, setUserDisplayName] = useState("");
+  const [userPhotoUrl, setUserPhotoUrl] = useState("");
+
+  /**
+   * Replace blank values with user details from DB
+   */
+  async function fetchUserData() {
+    let name = await getDisplayNameById(user.uid);
+    setUserDisplayName(name);
+    let url = await getPhotoUrlById(user.uid);
+    setUserPhotoUrl(url);
+  }
+
+  // Fetch user details on mount
+  useEffect(() => {
+    if (user) {
+      fetchUserData();
     }
+  }, []);
 
-    // Fetch user details on mount
-    useEffect(() => {
-        if (user) {
-            fetchUserData();
-        }
-    }, [])
-
-    /**
-     * Checks whether a user is completely signed in based on whether or not they have a display name
-     * @returns {Boolean} whether or not the user has completed signin process
-     */
-    function getSignedIn() {
-        if (user) {
-            if (user.displayName === null) {
-                return false;
-            } else {
-                return true;
-            }
-        } else {
-          return true;
-        }
+  /**
+   * Checks whether a user is completely signed in based on whether or not they have a display name
+   * @returns {Boolean} whether or not the user has completed signin process
+   */
+  function getSignedIn() {
+    if (user) {
+      if (user.displayName === null) {
+        return false;
+      } else {
+        return true;
       }
     } else {
       return false;
     }
   }
 
-    // Check if the user is signed in
-    const signedIn = getSignedIn();
+  // Check if the user is signed in
+  const signedIn = getSignedIn();
 
   /**
    * Log user out and redirect to homepage
@@ -108,91 +112,133 @@ export default function Topbar() {
     window.location = destination;
   }
 
-    /**
-     * Get a user's initials by first and last name
-     * @param {String} f first name
-     * @param {String} l last name
-     * @returns {String} initials
-     */
-    function getInitials() {
-        if (user) {
-            return userDisplayName.charAt(0);
-        } else {
-            // If we don't have a display name, they need to complete their profile!
-            if (!window.location.toString().includes("login")) {
-                window.location = "/login/account-creation"
-            }
-        }
+  /**
+   * Get a user's initials by first and last name
+   * @param {String} f first name
+   * @param {String} l last name
+   * @returns {String} initials
+   */
+  function getInitials() {
+    if (user) {
+      return userDisplayName.charAt(0);
+    } else {
+      // If we don't have a display name, they need to complete their profile!
+      if (!window.location.toString().includes("login")) {
+        window.location = "/login/account-creation";
+      }
     }
+  }
 
-    // Choose which topbar to display— signedIn is displays user information
-    if (signedIn) {
-        // Signed in, so set user vars and return detail topbar
-        return (
-            <div className="topbar" data-testid="topbar-wrapper">
-                <div className="appbar-container" data-testid="user-topbar">
-                    <AppBar className="appbar">
-                        <Toolbar>
-                            <IconButton size="large" edge="start" color="inherit" aria-label="logo" href="/home">
-                                <img src={BlackLogo} alt="logo" style={{width: "40px"}}></img>
-                            </IconButton>
-                            <Typography variant="h4" component="a" href="/home" fontFamily="FredokaOne" sx={{ flexGrow: 1 }}>
-                                Citrus
-                            </Typography>
-                            <Stack direction="row" spacing={2} alignItems="center" justifyContent="center" display="flex">
-                                <Typography variant="subtitle1" component="div" marginTop="4px">
-                                    {userDisplayName}
-                                </Typography>
-                                <IconButton aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={(e) => handleMenu(e)} color="inherit" data-testid="account-button">
-                                    <Avatar src={userPhotoUrl} alt={userDisplayName} sx={{ border: "1px solid black"}}>{getInitials()}</Avatar>
-                                </IconButton>
-                                <Menu 
-                                data-testid="account-menu"
-                                id="menu-appbar" 
-                                anchorEl={anchorElement}
-                                anchorOrigin={ 
-                                    { 
-                                        vertical: "bottom", 
-                                        horizontal: "left"
-                                    }
-                                }
-                                keepMounted
-                                transformOrigin={
-                                    {
-                                        vertical: "bottom",
-                                        horizontal: "left"
-                                    }
-                                }
-                                open={open}
-                                onClose={() => handleClose()}
-                                >
-                                    <MenuItem onClick={() => redirect("/user")}>
-                                        My Profile
-                                    </MenuItem>
-                                    <MenuItem onClick={() => redirect("/user/settings")}>
-                                        Settings
-                                    </MenuItem>
-                                </Menu>
-                                <Tooltip title="Notifications">
-                                    <IconButton size="medium" edge="start" color="inherit" aria-label="notifications-icon" href='#notifications'>
-                                        <NotificationsIcon />
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Bookmarks">
-                                    <IconButton size='medium' edge="start" color="inherit" aria-label="bookmarks-icon" href="#bookmarks">
-                                        <BookmarksIcon />
-                                    </IconButton>
-                                </Tooltip>
-                                <Tooltip title="Log-Out">
-                                    <IconButton size="medium" color="inherit" aria-label="logout-icon" onClick={() => logOut()} data-testid="topbar-logout-button">
-                                        <LogoutIcon/>
-                                    </IconButton>
-                                </Tooltip>
-                            </Stack>
-                        </Toolbar>
-                    </AppBar>
-                </div>
-            </div>
+  // Choose which topbar to display— signedIn is displays user information
+  if (signedIn) {
+    // Signed in, so set user vars and return detail topbar
+    return (
+      <div className="topbar" data-testid="topbar-wrapper">
+        <div className="appbar-container" data-testid="user-topbar">
+          <AppBar className="appbar">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="logo"
+                href="/home"
+              >
+                <img src={BlackLogo} alt="logo" style={{ width: "40px" }}></img>
+              </IconButton>
+              <Typography
+                variant="h4"
+                component="a"
+                href="/home"
+                fontFamily="FredokaOne"
+                sx={{ flexGrow: 1 }}
+              >
+                Citrus
+              </Typography>
+              <Stack
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+                display="flex"
+              >
+                <Typography variant="subtitle1" component="div" marginTop="4px">
+                  {userDisplayName}
+                </Typography>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={(e) => handleMenu(e)}
+                  color="inherit"
+                  data-testid="account-button"
+                >
+                  <Avatar
+                    src={userPhotoUrl}
+                    alt={userDisplayName}
+                    sx={{ border: "1px solid black" }}
+                  >
+                    {getInitials()}
+                  </Avatar>
+                </IconButton>
+                <Menu
+                  data-testid="account-menu"
+                  id="menu-appbar"
+                  anchorEl={anchorElement}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  open={open}
+                  onClose={() => handleClose()}
+                >
+                  <MenuItem onClick={() => redirect("/user")}>
+                    My Profile
+                  </MenuItem>
+                  <MenuItem onClick={() => redirect("/user/settings")}>
+                    Settings
+                  </MenuItem>
+                </Menu>
+                <Tooltip title="Notifications">
+                  <IconButton
+                    size="medium"
+                    edge="start"
+                    color="inherit"
+                    aria-label="notifications-icon"
+                    href="#notifications"
+                  >
+                    <NotificationsIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Bookmarks">
+                  <IconButton
+                    size="medium"
+                    edge="start"
+                    color="inherit"
+                    aria-label="bookmarks-icon"
+                    href="#bookmarks"
+                  >
+                    <BookmarksIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Log-Out">
+                  <IconButton
+                    size="medium"
+                    color="inherit"
+                    aria-label="logout-icon"
+                    onClick={() => logOut()}
+                    data-testid="topbar-logout-button"
+                  >
+                    <LogoutIcon />
+                  </IconButton>
+                </Tooltip>
+              </Stack>
+            </Toolbar>
           </AppBar>
         </div>
       </div>
