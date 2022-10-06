195057248511b8929c75432e38795973dfe6be77
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-10-06 13:36:34 -0400

    Statics are now in Server directory

--- server.js
+++ server.js
@@ -19,45 +19,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Serve static files
-app.use(express.static(__dirname + "/static/"));
+app.use(express.static(__dirname + "/server/static/"));
 app.get("/credits", (req, res) => {
-    res.sendFile(__dirname + "/static/credits.html");
+    res.sendFile(__dirname + "/server/static/credits.html");
 })
 app.get("/features", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/apps", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/pricing", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/faq", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/billing", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/roadmap", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/blog", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/support", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/status", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/privacy", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/terms", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 app.get("/eula", (req, res) => {
-    res.sendFile(__dirname + "/static/pageUnderConstruction.html");
+    res.sendFile(__dirname + "/server/static/pageUnderConstruction.html");
 })
 
 // Serve React build
